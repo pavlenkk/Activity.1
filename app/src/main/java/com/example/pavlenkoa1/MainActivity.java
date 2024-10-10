64e8d9b52909b171209e1ed61f047dd0eb4e0976
pavlenkk <aliana.pavlenko@yandex.ru>
2024-10-10 11:24:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pavlenkoa1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.pavlenkoa1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(v -> {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("surname", "Павленко");
+                startActivity(intent);
+        });
+    }
+}
